b251b5d7d6ee6203958678855b4ba5a2d881573e
Ismaïl BENHALLAM <ismailben44@gmail.com>
2021-08-05 12:11:27 +0100

    Add button to save the resume in PDF format

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { PureComponent } from "react";
 import "./App.css";
 import Certifications from "./components/certifications/Certifications";
 import Details from "./components/details/Details";
@@ -7,24 +10,43 @@ import Education from "./components/realisations/education/Education";
 import Experiences from "./components/realisations/experiences/Experiences";
 import Skills from "./components/skills/Skills";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="left">
-        <PhotoAndName />
-        <Details />
-      </div>
-      <div className="right">
-        <Experiences />
-        <Divider />
-        <Education />
-        <Divider />
-        <Skills />
-        <Divider />
-        <Certifications />
-      </div>
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}>
+          <button
+            className="saveButton"
+            tabIndex={1}
+            onClick={() => window.print()}>
+            Save as PDF&nbsp;
+            <FontAwesomeIcon icon={faFilePdf} className="icon" />
+          </button>
+        </div>
+        <div id="resume" className="container">
+          <div className="left">
+            <PhotoAndName />
+            <Details />
+          </div>
+          <div className="right">
+            <Experiences />
+            <Divider />
+            <Education />
+            <Divider />
+            <Skills />
+            <Divider />
+            <Certifications />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
