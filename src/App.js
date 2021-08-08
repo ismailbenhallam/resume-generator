3dd789259dd4c5811dd1fda317bce604c4c2cde3
Ismaïl BENHALLAM <ismailben44@gmail.com>
2021-08-08 19:44:08 +0100

    Add <Select> to change the chosen template

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PureComponent } from "react";
 import "./App.css";
+import Template1 from "./templates/template1/Template1";
 import Template2 from "./templates/template2/Template2";
-// import Template1 from "./templates/template1/Template1";
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      templates: {
+        template1: <Template1 />,
+        template2: <Template2 />,
+      },
+    };
+    this.state.selectedTemplate = this.state.templates.template1;
+  }
   render() {
     return (
       <>
+        <select onChange={this.handleChangeTemplate}>
+          {Object.entries(this.state.templates).map((entry) => (
+            <option value={entry[0]}>{entry[0]}</option>
+          ))}
+        </select>
         <div className="save-button-wrapper">
           <button
             className="save-button"
@@ -19,11 +34,17 @@ class App extends PureComponent {
           </button>
         </div>
         <div id="resume" className="container">
-          <Template2 />
+          {this.state.selectedTemplate}
         </div>
       </>
     );
   }
+
+  handleChangeTemplate = (event) => {
+    this.setState({
+      selectedTemplate: this.state.templates[event.target.value],
+    });
+  };
 }
 
 class ErrorBoundaries extends PureComponent {
